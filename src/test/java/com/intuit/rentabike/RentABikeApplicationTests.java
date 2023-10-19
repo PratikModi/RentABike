cef6ac97e03dc614c832c131d6b20ebe4b18ad39
Pratik1 <pratik.modi@gmail.com>
2023-10-19 19:45:46 +0530

    Initial commit

---
+++ src/test/java/com/intuit/rentabike/RentABikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit.rentabike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentABikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
